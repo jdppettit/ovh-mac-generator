0a3d0aea27ac8f7974d88ad2a777004abe02cf9f
Joe Pettit <jpettit@pettitservers.com>
2016-02-15 22:00:23 -0600

    ip_functions vastly improved thans to buhman

--- generator.js
+++ generator.js
@@ -11,16 +11,19 @@ var ovh = require('ovh')({
 
 var ip_network = process.env.IP_RANGE;
 var ip_array = ip_functions.enumerate(ip_network);
-
+console.log(ip_array);
 function requestVirtualMac(ip_address) {
-  ovh.request('POST', '/dedicated/server/' + config.SERVICE_NAME, {
+  ovh.request('POST', '/dedicated/server/' + config.SERVICE_NAME + '/virtualMac', {
     ipAddress: ip_address,
     type: 'ovh',
     virtualMachineName: uuid.v4()
   }, function(error, result) {
     if (error) {
       throw error;
+      console.log(error);
     } else {
+     console.log("OVH is now working on a MAC for " + ip_address + "...");
+     console.log(result);
       return checkRequestStatus(result);
     }
   });
@@ -34,15 +37,18 @@ function checkRequestStatus(request) {
       } else {
         console.log(response);
         return Promise.delay(5000).then(function() {
-          checkRequestStatus(request);
+          return checkRequestStatus(request);
         });
       }
     });
   });
 }
-
+/*
 Promise.map(ip_array, function(ip) {
+ console.log(ip);
   return requestVirtualMac(ip);
-}).then(function() {
+}, {concurrency: 3}).then(function() {
   console.log("Done");
-});
+});*/
+
+requestVirtualMac('167.114.22.1');
