3f98eb19d1eb20fc7f8a9908feed7309f8bec852
Joe Pettit <jpettit@pettitservers.com>
2016-02-16 15:10:58 -0600

    Added lib/ip_functions, and most of the code needed in generator.js

--- authorize.js
+++ authorize.js
@@ -1,27 +1,30 @@
 var config = require('./config');
 var ovh = require('ovh')({
-	endpoint: config.ENDPOINT,
-	appKey: config.APP_KEY,
-	appSecret: config.APP_SECRET
+  endpoint: config.ENDPOINT,
+  appKey: config.APP_KEY,
+  appSecret: config.APP_SECRET
 });
-
+console.log("This utility will allow you to authorize this application on your OVH account...");
 ovh.request('POST', '/auth/credential', {
-	'accessRules': [
-		{ 'method': 'GET', 'path': '/*' },
-		{ 'method': 'POST', 'path': '/*' }
-	]
+  'accessRules': [{
+    'method': 'GET',
+    'path': '/*'
+  }, {
+    'method': 'POST',
+    'path': '/*'
+  }]
 }, function(error, credential) {
-	if (error) {
-		console.log("Got an error...");
-		console.error(error);
-		process.exit(1);
-	} else {
-		console.log("Success!");
-		console.log("Please do the following:");
-		console.log("1. Take the consumerKey and add it to config.js");
-		console.log("2. Visit the validationUrl and authorize this application");
-		console.log("3. Run node IP_RANGE=$YOURNETWORKRANGE generator.js");
-		console.log(credential);
-		process.exit(0);
-	}
+  if (error) {
+    console.log("Got an error...");
+    console.error(error);
+    process.exit(1);
+  } else {
+    console.log("Success!");
+    console.log("Please do the following:");
+    console.log("1. Take the consumerKey and add it to config.js");
+    console.log("2. Visit the validationUrl and authorize this application");
+    console.log("3. Run node IP_RANGE=$YOURNETWORKRANGE generator.js");
+    console.log(credential);
+    process.exit(0);
+  }
 });
--- generator.js
+++ generator.js
@@ -1,4 +1,48 @@
 var config = require('./config');
+var ip_functions = require('./lib/ip_functions');
+var uuid = require('uuid');
+var Promise = require('bluebird');
 var ovh = require('ovh')({
-  
+  appKey: config.APP_KEY,
+  appSecret: config.APP_SECRET,
+  consumerKey: config.CONSUMER_KEY,
+  endpoint: config.ENDPOINT
+});
+
+var ip_network = process.env.IP_RANGE;
+var ip_array = ip_functions.enummerate(ip_network);
+
+function requestVirtualMac(ip_address) {
+  ovh.request('POST', '/dedicated/server/' + config.SERVICE_NAME, {
+    ipAddress: ip_address,
+    type: 'ovh',
+    virtualMachineName: uuid.v4()
+  }, function(error, result) {
+    if (error) {
+      throw error;
+    } else {
+      return checkRequestStatus(result);
+    }
+  });
+}
+
+function checkRequestStatus(request) {
+  return new Promise(function(resolve) {
+    ovh.request('GET', '/dedicated/server' + config.SERVICE_NAME + '/task/' + request.properties.taskId.description, function(error, response) {
+      if (error) {
+        throw error;
+      } else {
+        console.log(response);
+        return Promise.delay(5000).then(function() {
+          checkRequestStatus(request);
+        });
+      }
+    });
+  });
+}
+
+Promise.map(ip_array, function(ip) {
+  return requestVirtualMac(ip);
+}).then(function() {
+  console.log("Done");
 });
